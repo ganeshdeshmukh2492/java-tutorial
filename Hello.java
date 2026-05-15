a50e3d0f4bd4d63d941d61e1e031f50f35e72398
ganeshdeshmukh2492 <deshmukhganesh2492@gmail.com>
2026-05-15 12:37:05 +0530

    new

--- Hello.java
+++ Hello.java
@@ -1,5 +1,22 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        // System.out.println("hello, world");
+        int num1=10;
+        // int num2=20;
+        //  num1 +=5;
+        // System.out.println( result);
+        // int result=num1+=5;
+        // Increment
+        // num1++;
+        // decrement
+        // num1--;
+        int x=5;
+        int y=6;
+        Boolean result= x!=y;
+        // relational operator
+        // ==. >=,=< =! ,>,< ===
+        // logical operator
+        System.out.println(result);
+    
     }
 }
