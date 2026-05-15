e6cfea922177921cd727a25c32ad3517a86780f5
ganeshdeshmukh2492 <deshmukhganesh2492@gmail.com>
2026-05-16 00:17:04 +0530

    new

--- Hello.java
+++ Hello.java
@@ -22,15 +22,48 @@ class Hello {
         // int a=2;
         // int b=4;
         // Boolean result = x<y || a<b;
-        int x=5;
-        int y=7;
-        if(x >y && x <y)
-             System.out.println(x);
-        else
-            System.out.println(y);
+        // if statement
+        // int x=5;
+        // int y=7;
+        // if(x >y && x <y)
+            //  System.out.println(x);
+        // else
+            // System.out.println(y);
 
         
         // System.out.println(result);
     
+    // if else if 
+    // 
+    // ternary operator
+    // int n=5;
+    // int result=0;
+    // result= n%2==0 ? 10 :20;
+    // System.out.println(result);
+    // switch statement
+    // 
+    
+
+    // loops in java
+    // while loop
+    // do while loop
+    // for loops
+ 
+    //  int i=1;
+    // while(i<=5){
+    // System.out.println( "hi"+i);
+    // i++;
+    // }
+    int i=1;
+    while(i<=5){
+        System.out.println("hii"+1);
+    
+    int j=1;
+    while(j<=5){
+       System.out.println("hello"+j);
+       j++;
+    }
+    i++;
+    }
     }
 }
