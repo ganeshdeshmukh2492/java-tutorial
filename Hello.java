3e08b6c1507a408ed12ed2875f9695e514f4b05f
ganeshdeshmukh2492 <deshmukhganesh2492@gmail.com>
2026-05-15 15:40:56 +0530

    newchange

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String[] args) {
         // System.out.println("hello, world");
-        int num1=10;
+        // int num1=10;
         // int num2=20;
         //  num1 +=5;
         // System.out.println( result);
@@ -10,13 +10,27 @@ class Hello {
         // num1++;
         // decrement
         // num1--;
-        int x=5;
-        int y=6;
-        Boolean result= x!=y;
+        // int x=5;
+        // int y=6;
+        // Boolean result= x!=y;
         // relational operator
         // ==. >=,=< =! ,>,< ===
         // logical operator
-        System.out.println(result);
+        // AND-&,OR-|,!-not
+        // int x=5;
+        // int y=6;
+        // int a=2;
+        // int b=4;
+        // Boolean result = x<y || a<b;
+        int x=5;
+        int y=7;
+        if(x >y && x <y)
+             System.out.println(x);
+        else
+            System.out.println(y);
+
+        
+        // System.out.println(result);
     
     }
 }
