628c7cedf982030a1c735585f25311a11d441c62
ganeshdeshmukh2492 <deshmukhganesh2492@gmail.com>
2026-06-19 19:15:02 +0530

    new

--- Hello.java
+++ Hello.java
@@ -556,3 +556,27 @@
 
 //     }
 // }
+
+class Calc{
+
+    public int add(int n1,int n2 ){
+        return n1 +n2;
+    }
+    public int sub(int n1 ,int n2){
+        return n1-n2;
+    }
+}
+
+
+public class hello{
+    public static void main (String[] args)
+    {
+        Calc obj= new Calc();
+        int r1=obj.add(20,30);
+        int r2=obj.sub(30,20);
+
+        System.out.println(r1+" "+r2);
+
+
+    }
+}
